d1be7ebad032392013040c6523bb0474fa410242
angelinara <106839384+angelinara@users.noreply.github.com>
2026-06-09 15:57:58 +0100

    added Redistributor empty methods

--- src/consistenthashing/Redistributor.java
+++ src/consistenthashing/Redistributor.java
@@ -1,5 +1,24 @@
 package consistenthashing;
 
+import java.util.List;
+import java.util.TreeMap;
+
 // TODO:
 public class Redistributor {
+
+    public static List<RangeChange> afterAdd(TreeMap<Long, VirtualNode> before,
+            TreeMap<Long, VirtualNode> after,
+            Node addedNode) {
+
+        return null;
+
+    }
+
+    public static List<RangeChange> afterRemove(TreeMap<Long, VirtualNode> before,
+            TreeMap<Long, VirtualNode> after,
+            Node removedNode) {
+                
+        return null;
+    }
+
 }
